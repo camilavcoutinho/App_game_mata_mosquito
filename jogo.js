10e07dd38d7e2b419150c05aae3ce1e7216c12ea
Camila Vieira Coutinho <camila.coutinho@vtex.com.br>
2023-01-19 18:07:55 -0300

    criando posicoes randomicas

--- jogo.js
+++ jogo.js
@@ -8,6 +8,20 @@ function ajustaTamanhoPalcoJogo(){
 
 ajustaTamanhoPalcoJogo()
 
+function posicaoRandomica(){
+    var posicaoX = Math.floor(Math.random() * largura) - 90 //criando posicao randomica no eixo x, arredondando para baixo
+    var posicaoY = Math.floor(Math.random() * altura) - 90//criando posicao randomica no eixo y, arredondando para baixo
 
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
 
-console.log (altura, largura)
+    //criar o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png' 
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px' 
+    mosquito.style.top = posicaoY + 'px' 
+    mosquito.style.position = 'absolute' 
+
+    document.body.appendChild(mosquito)
+}
