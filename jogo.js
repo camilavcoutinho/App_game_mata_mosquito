a47c492ee7b29526d4f3c3ca1a0e0b7c8c33be5c
Camila Vieira Coutinho <camila.coutinho@vtex.com.br>
2023-01-21 17:11:27 -0300

    controlando os pontos de vida

--- jogo.js
+++ jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
@@ -13,7 +14,13 @@ function posicaoRandomica(){
     //remover o mosquito anterior (caso exista)
     if(document.getElementById('mosquito')){
         document.getElementById('mosquito').remove()
-    }
+        if(vidas > 3){ 
+
+        } else{
+        document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+        vidas++
+        }
+    }  
 
     var posicaoX = Math.floor(Math.random() * largura) - 90 //criando posicao randomica no eixo x, arredondando para baixo
     var posicaoY = Math.floor(Math.random() * altura) - 90//criando posicao randomica no eixo y, arredondando para baixo
@@ -29,6 +36,9 @@ function posicaoRandomica(){
     mosquito.style.top = posicaoY + 'px' 
     mosquito.style.position = 'absolute' 
     mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
 }
